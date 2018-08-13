0ad639e96844fd61f3a5615876597297e4770f29
dcarob <30913899+dcarob@users.noreply.github.com>
2018-08-13 15:46:24 -0500

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,32 @@
+from django.conf.urls import url
+from . import views
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.contrib.auth.views import login
+
+urlpatterns= [
+    url(r'^profile/$',views.userprofile,name='profile'),
+    url(r'^register/$',views.register.as_view(),name='register'),
+    url(r'^login/$',views.logout,name='login'),
+    url(r'^edit/$',views.editprofile,name='edit'),
+    url(r'^groups/$', views.ProfileList.as_view(), name='profile-list'),
+    url(r'^newgroup/$',views.GroupGroupMemberCreate.as_view(),name='GroupGroupMemberCreate'),
+    url(r'^newgroupexcel/$',views.loadExcel,name='newexcelgroup'),
+    url(r'^chooseSheet/$',views.chooseSheet,name='chooseSheet'),
+    url(r'^pickColumns/$',views.pickcolumns,name='pickcolumns'),
+    url(r'^formset_excel/$',views.formset_excel.as_view(),name='formset_excel'),
+
+    url(r'^$', login, {'template_name': 'pfapp/login.html'}, name='login'),
+    url(r'^edit/password/$',views.change_password,name='changepassword'),
+    url(r'^prueba/$',views.codificacion ,name='photo'),
+    url(r'^(?P<group_grupo>[0-9]+)/$',views.GroupList, name='detail'),
+    url(r'^upload/$', views.GroupPhotoEntry.as_view(), name='upload-photo'),
+    url(r'^uppicture/$', views.pictureUpload, name='picture-upload'),
+    url(r'^attendance/$',views.attendanceGenerator ,name='attendance'),
+    url(r'^editgroup/$',views.editGroup.as_view(),name='editgroup'),
+
+
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
